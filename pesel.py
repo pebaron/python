4bd4ab7207ee147ad1dcf019142881a3f0c439b1
Petr Baron <petr.baron@cern.ch>
2020-11-08 21:17:30 +0100

    pesel update

--- pesel.py
+++ pesel.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 CheckSum = np.array([1,3,7,9,1,3,7,9,1,3])
-Population = 40000000
+Population = 4000000
 
 def CheckPESEL(PESEL):
     checksum = 0
@@ -15,23 +15,23 @@ def CheckPESEL(PESEL):
         return True
     else: 
         return False
+print("DEBUG 1")
 
 dates = np.array('1800-01-01', dtype=np.datetime64)
 dates = dates + np.arange(365*500 + 121)
-print(dates)
-
+print("DEBUG 2")
 #dates = pd.date_range(start='1/1/1800', end='1/1/2100')
 dates = np.datetime_as_string(dates, unit='D')
 dates = np.core.defchararray.replace(dates, '-','')
 dates = np.array( dates, dtype=np.int)
-CheckDates = dates
-
+#CheckDates = dates
+print("DEBUG 3")
 dates = np.where( (dates >= 18000000)&(dates <= 18999999), dates-18000000 + 8000, dates) 
 dates = np.where( (dates >= 19000000)&(dates <= 19999999), dates-19000000       , dates) 
 dates = np.where( (dates >= 20000000)&(dates <= 20999999), dates-20000000 + 2000, dates) 
 dates = np.where( (dates >= 21000000)&(dates <= 21999999), dates-21000000 + 4000, dates) 
 dates = np.where( (dates >= 22000000)&(dates <= 22999999), dates-22000000 + 6000, dates)
-
+print("DEBUG 4")
 
 #print("Real Date: ", CheckDates[1] , " Pesl Date: ", dates[1])
 #print("Real Date: ", CheckDates[50000] , " Pesl Date: ", dates[50000])
@@ -45,6 +45,8 @@ RandomPESEL = np.array( RandomPESEL, dtype=np.str)
 RandomPESEL = np.where( np.char.str_len(RandomPESEL) == 3, np.char.add('000',RandomPESEL) , RandomPESEL)
 RandomPESEL = np.where( np.char.str_len(RandomPESEL) == 4, np.char.add('00',RandomPESEL) , RandomPESEL)
 RandomPESEL = np.where( np.char.str_len(RandomPESEL) == 5, np.char.add('0',RandomPESEL) , RandomPESEL)
+
+print("DEBUG 5")
 Z1 = np.random.randint(0,9,size=Population)
 Z2 = np.random.randint(0,9,size=Population)
 Z3 = np.random.randint(0,9,size=Population)
@@ -57,7 +59,7 @@ RandomPESEL = np.char.add(RandomPESEL,Z1)
 RandomPESEL = np.char.add(RandomPESEL,Z2)
 RandomPESEL = np.char.add(RandomPESEL,Z3)
 RandomPESEL = np.char.add(RandomPESEL,X4)
-
+print("DEBUG 6")
 
 
 Digit1 = np.array(RandomPESEL.view('<U1').reshape(RandomPESEL.shape + (-1,))[..., 0], dtype=np.int)
@@ -70,7 +72,7 @@ Digit7 = np.array(RandomPESEL.view('<U1').reshape(RandomPESEL.shape + (-1,))[...
 Digit8 = np.array(RandomPESEL.view('<U1').reshape(RandomPESEL.shape + (-1,))[..., 7], dtype=np.int)
 Digit9 = np.array(RandomPESEL.view('<U1').reshape(RandomPESEL.shape + (-1,))[..., 8], dtype=np.int)
 Digit10 = np.array(RandomPESEL.view('<U1').reshape(RandomPESEL.shape + (-1,))[..., 9], dtype=np.int)
-
+print("DEBUG 7")
 #print("Digit1 : ",  Digit1)
 #print("Digit2 : ",  Digit2)
 #print("Digit3 : ",  Digit3)
@@ -93,7 +95,7 @@ SumDigit7 = Digit7  * CheckSum[6]
 SumDigit8 = Digit8  * CheckSum[7]
 SumDigit9 = Digit9  * CheckSum[8]
 SumDigit10 = Digit10 * CheckSum[9]
-
+print("DEBUG 8")
 #print("Digit1 : ",  Digit1)
 #print("Digit2 : ",  Digit2)
 #print("Digit3 : ",  Digit3)
@@ -116,6 +118,7 @@ RandomPESEL = np.unique(RandomPESEL)
 Vibration = np.array(Digit1 + Digit2 + Digit3 + Digit4 + Digit5 + Digit6 + Digit7 + Digit8 + Digit9 + Digit10)
 VibrationString = np.array(Vibration, dtype=np.str)
 print(VibrationString.shape)
+print("DEBUG 9")
 VibrationString1 = VibrationString[ np.char.str_len(VibrationString) == 1]
 VibrationString2 = VibrationString[ np.char.str_len(VibrationString) == 2]
 
@@ -127,6 +130,10 @@ VibDigit2 = np.array(VibrationString2.view('<U1').reshape(VibrationString2.shape
 Vibration2 = VibDigit1 + VibDigit2
 
 from matplotlib import pyplot as plt
-
+print("DEBUG 10")
 plt.hist(np.append(Vibration2, Vibration1)) 
+print("DEBUG 11")
+plt.xlabel("Vibration of PESEL")
+plt.ylabel("Number of PESELs")
+plt.title("Vibration distribution of PESEL number from years 1800-2299")
 plt.show()
